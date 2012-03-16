d3a11a3375143aad35bc0ecf87f4c2ef5511527c
Jesse Tane <jesse.tane@gmail.com>
2012-03-16 10:18:11 -0700

    working on map

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-
-/**
- * Module dependencies.
+/*
+ *	Jobsite
+ *
  */
 
+var port = 1337;
+var stylus = require('stylus');
 var express = require('express');
-
+var expose = require('express-expose');
 var app = module.exports = express.createServer();
+
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+	app.exposeRequire();
+	app.exposeModule(__dirname + '/node_modules/backbone', 'backbone');
+	app.exposeModule(__dirname + '/node_modules/underscore', 'underscore');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(stylus.middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	serve();
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
+	port = 80;
+	serve();
+	process.setuid("jessetane");  // or server user
 });
+
 
 // Routes
 
@@ -36,5 +47,16 @@ app.get('/', function (req, res) {
 	});
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.get('/map', function (req, res) {
+	res.render('map', {
+		layout: null
+	});
+});
+
+
+// Functions
+
+function serve () {
+	app.listen(port);
+	console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+}
